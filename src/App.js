e5b98cd461ef9df8af25d25b48679dfbb3f87437
Richard Westmoreland <kegtrack@gmail.com>
2019-09-08 09:57:47 -0500

    Initial commit and update readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "06678112"
+  const APP_KEY = "f3eb988da60219a026750222a81f6148"
+  const API_URL = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+
+  useEffect(() => {
+
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar"></input>
+        <button className="search-button">search</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
